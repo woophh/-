d24348198afa536acf6ccf4aa9684f4a1ec46cba
woophh <odoeva.karina@mail.ru>
2025-02-20 22:39:11 +0300

    анимация бургера

--- src/JS/burger.js
+++ src/JS/burger.js
@@ -4,13 +4,18 @@ const burgerLinks = document.querySelector('#burgerLinks');
 const burgerLink = document.querySelector('#burgerLink');
 
 burgerBtn.addEventListener('click', () => {
-    burgerMenu.classList.toggle('burger-active')
-})
+  burgerMenu.classList.toggle('burger-active');
+  if (burgerMenu.classList.contains('burger-active')) {
+    document.body.classList.add('stop-scrolling');
+  } else {
+    document.body.classList.remove('stop-scrolling');
+  }
+});
 
 burgerLinks.addEventListener('click', () => {
-    burgerMenu.classList.remove('burger-active')
-})
+  burgerMenu.classList.remove('burger-active');
+});
 
 burgerLink.addEventListener('click', () => {
-    burgerMenu.classList.remove('burger-active')
-})
+  burgerMenu.classList.remove('burger-active');
+});
